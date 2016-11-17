25813132b220f40d0270a981dbd8cde1e823a203
Long Doan <hoanglong7421@gmail.com>
2016-11-17 22:09:17 +0700

    change method get ip

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ var port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'public/templates'));
 app.set('view engine', 'jade');
 app.get('/', function(req, res) {
-	var ip = req.headers['host'];
+	var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
 	var language = req.headers['accept-language'].slice(0,5);
 	var sys = req.headers['user-agent'];
-	ip = ip.slice(0,ip.indexOf(':'));
-	sys = sys.match(/\(.*?\)/);
-	sys = sys.toString().slice(1,-1);
+	ip = ip.match(/.*:(.*?)$/)[1];
+	sys = sys.match(/\((.*?)\)/)[1];
 	res.render('index', {
 		ip: ip,
 		language: language,
