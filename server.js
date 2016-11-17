8e74a9a333f391ff5fc5a793e04577e5995f0b67
Long Doan <hoanglong7421@gmail.com>
2016-11-17 23:21:59 +0700

    get real ip

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ var port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'public/templates'));
 app.set('view engine', 'jade');
 app.get('/', function(req, res) {
-	var ip = req.connection.remoteAddress; console.log('ip', ip);
+	var ip = req.headers["x-forwarded-for"];
+  	if (ip){
+    	var list = ip.split(",");
+    	ip = list[list.length-1];
+  	} else {
+    	ip = req.connection.remoteAddress;
+  	} 
+	console.log('ip', ip);
 	var language = req.headers['accept-language'].slice(0,5);
 	var sys = req.headers['user-agent'];
 	ip = ip.match(/.*:(.*?)$/)[1];
